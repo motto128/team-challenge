22880b2e62fb11933f47bd47fcad584e0fae7703
jorgetorres26 <jorgetor1126@gmail.com>
2016-11-28 22:14:47 -0800

    Commenting added

--- test-app/src/App.test.js
+++ test-app/src/App.test.js
@@ -24,17 +24,17 @@ describe ('<SignUpForm/> component', () =>{
 // Jorge's tests
 
 describe ('<EmailInput/> component', () =>{
-
+// Verify that email field contains a string with an '@' inbetween
   it('Valid input scenario: no error message', () => {
     const wrapper = shallow(<EmailInput value={'a@a.com'} updateParent={update} />);
     expect(wrapper.children().length).toEqual(2);
   })
-
+// Verify that if nothing is typed in, show an error message
   it ('Blank email scenario: should show error message' , () =>{
     const wrapper = shallow(<EmailInput value={''} updateParent={update}  />);
     expect(wrapper.find('p').text()).toEqual('we need to know your email address');
   })
-
+// When there is an invalid email entered, show error message
   it('Invalid email scenario: should show error message', () => {
     const wrapper = shallow(<EmailInput value={'invalid email'} updateParent={update} />);
     expect(wrapper.find('p').text()).toEqual('this is not a valid email address'); 
@@ -44,7 +44,7 @@ describe ('<EmailInput/> component', () =>{
 })
 
 describe('<RequiredInput /> component', () => {
-
+// When there is a blank input, show error message
     it('Blank input scenario: should show error message', () => {
         const wrapper = shallow(<RequiredInput id="name" field="name" type="text" label="Name" placeholder="your name"
         errorMessage="we need to know your name" value={''} updateParent={update} />);
